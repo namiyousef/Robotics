f4af7b9a29c9eea2ff2761b81354f515ba6934b3
Yousef <namiyousef@hotmail.com>
2020-10-25 17:48:35 +0000

    modified to work with OOP

--- Exercises/Chapter1/Ex6.m
+++ Exercises/Chapter1/Ex6.m
@@ -4,7 +4,14 @@
 % Purpose: create a function HT that implements the homogeneous transform
 % operator from frame {B} to frame {A}
 
-A_p = HT([cosd(30), -sind(30), 0; sind(30), cosd(30), 0; 0, 0, 1], [1,2,3], [2,3,3]);
+% the function has been created within the file transformation_operator you
+% this file just tests it
+obj = transformation_operator;
+
+% run function
+A_p = obj.HT([cosd(30), -sind(30), 0; sind(30), cosd(30), 0; 0, 0, 1], [1,2,3], [2,3,3]);
+
+%% for testing purpose
 hold on
 plot([0,1], [0,2]);
 plot([1,3],[2,5]);
@@ -14,22 +21,5 @@ Rot = [cosd(30), -sind(30); sind(30), cosd(30)];
 v = [2,3]';
 New_vec = Rot * v;
 plot([1,1+New_vec(1)],[2, 2+New_vec(2)]);
-%% Function definition
 
-function A_p = HT(AB_R, AB_0, B_p)
-dof = length(AB_R);
-AB_T = eye(dof + 1);
-AB_T(1:dof, 1:dof) = AB_R;
-AB_T(1:dof,dof + 1) = AB_0;
-P = [];
-for i = 1:dof
-    P(i) = B_p(i);
-end
-P(dof + 1) = 1;
-P = P';
-A_p = AB_T * P;
-disp(AB_T);
-disp(P);
-disp(A_p);
-end
 
