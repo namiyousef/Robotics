774350646f3935d1499583eaecb0722eca5a8e4d
Yousef <namiyousef@hotmail.com>
2021-04-13 10:02:46 +0200

    #1 corrected the forward kinematics

--- Coursework/find_theta1.m
+++ Coursework/find_theta1.m
@@ -1,4 +1,7 @@
-function theta1 = find_theta1(x, y, L1, Le, phi, d2)
-lambda = (L1+d2)*cos(phi);
-theta1 = atan2(Le*y - lambda*x, Le*x + lambda*x);
+function theta1 = find_theta1(x, y, L1, Le, phi, d2, gamma)
+l = (L1+d2)*cos(phi);
+%theta1 = atan2(Le*y - l*x, Le*x + l*y);
+%theta1 = atan2((Le*y - l*x),(Le*x + l*y));
+theta1 = asin((x - Le*cos(gamma))/l);
+
 end
